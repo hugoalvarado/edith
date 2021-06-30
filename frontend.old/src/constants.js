36bb62db93d2f4744e713fa6df65ffa39aa62f9f
Hugo Alvarado <hugo@britecore.com>
2021-06-29 23:22:58 -0600

    Remove default frontend

--- frontend.old/src/constants.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_TOKEN_COOKIE_NAME = 'jwt'
-export const DATETIME_FORMAT = 'MMMM Do YYYY, h:mm a'
-export const DATE_FORMAT = 'MMMM Do YYYY'
-
-export const EDITOR_MODE_HTML = 'html'
-export const EDITOR_MODE_MARKDOWN = 'markdown'
